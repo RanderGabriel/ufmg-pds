f79ddf54c4f2648d0bcf175daf03769f346e748c
theaikau <brianaikau@gmail.com>
2020-03-12 00:50:45 -0300

    Projeto inicial do frontend.

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
