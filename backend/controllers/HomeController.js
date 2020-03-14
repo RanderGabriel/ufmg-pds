7b3338ccbd6a7d0b0982eb06510e09493f249935
RanderGabriel <randergabriel98@gmail.com>
2020-03-13 23:41:37 -0300

    Removido código gerado

--- backend/controllers/HomeController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var router = express.Router();
-router.get('/', function (req, res) {
-    res.sendFile('index.html', { root: 'public' });
-});
-exports["default"] = router;
