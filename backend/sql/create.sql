1b51bb2d747ee8cda270576e764ac2065a072c5c
RanderGabriel <randergabriel98@gmail.com>
2020-03-15 09:00:57 -0300

    Adicionada foreign key constraint no campo User.profile e populada tabela Profile com valores para perfil "MECHANIC"

--- backend/sql/create.sql
+++ backend/sql/create.sql
@@ -1,16 +1,20 @@
 CREATE DATABASE IF NOT EXISTS pds_mec;
 
+CREATE TABLE IF NOT EXISTS pds_mec.Profile (
+    name varchar(255) NOT NULL PRIMARY KEY
+);
+
 CREATE TABLE IF NOT EXISTS pds_mec.User (
     email varchar(255) NOT NULL PRIMARY KEY,
     passwordHash varchar(255) NOT NULL,
     profile varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS pds_mec.Profile (
-    name varchar(255) NOT NULL PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS pds_mec.Mechanic (
     userEmail varchar(255) NOT NULL PRIMARY KEY,
     FOREIGN KEY (userEmail) REFERENCES User(email)
 );
+
+ALTER TABLE pds_mec.User ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`profile`) REFERENCES `profile` (`name`);
+
+INSERT IGNORE INTO pds_mec.Profile values ("MECHANIC");
