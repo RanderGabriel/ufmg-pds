3b00e3e6bbd5437095d9cd8a23961a4f50598f3a
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-03-17 20:51:53 -0300

    cors

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -10,11 +10,11 @@ router.use(express.json());
 router.use(LoginRouter);
 router.use(MechanicRouter);
 router.use(DriverRouter);
-// router.use((req : express.Request, res: express.Response, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-//     next();
-// });
+router.use((req : express.Request, res: express.Response, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
 
 export default router;
