01ee093a07dd50451b52f3fd278c92889c487f98
Eduardo Oliveira <eduveraneio@gmail.com>
2020-03-18 11:38:08 -0300

    Refatoração de Recuperar dados

--- backend/src/controllers/Login.ts
+++ backend/src/controllers/Login.ts
@@ -31,34 +31,27 @@ export class LoginController{
     }
 
     async reset(req :express.Request, res: express.Response) {
-        createConnection().then(async connection => {
-            const { email } = req.body;
-            const userRepository = connection.getRepository(User);
-            
-            const user = await userRepository.findOne({ email });
-            if(user === undefined){
-                connection.close();
-                res.status(400).send({ error: 'Usuário não encontrado, tente novamente.' });
-            }
-            else{
-                const token = crypto.randomBytes(20).toString('hex');
+        const { email } = req.body;
+        const connection = await createConnection();
+        const userRepository = connection.getRepository(User);
+        const user = await userRepository.findOne({ email });
+        if(user === undefined) {
+            res.status(500).send({ success: false, message: "Usuário não encontrado" });
+            connection.close();
+            return;
+        } else {
+            const token = crypto.randomBytes(20).toString('hex');
+            const now: Date = new Date();
+            now.setHours(now.getHours() + 1);
 
-                const now: Date = new Date();
-                now.setHours(now.getHours() + 1);
+            user.passwordResetToken = token;
+            user.passwordResetExpires = now;
+            await userRepository.save(user);
 
-                user.passwordResetToken = token;
-                user.passwordResetExpires = now;
-
-                await userRepository.save(user);
-
-                connection.close();
-                res.status(200).send({success: true, user});
-            }
-        })
-        .catch(err => {
-                console.log(err);
-                res.status(400).send({ success: false, err });
-        })
+            connection.close();
+            res.status(200).send({success: true, user});
+        }    
+        connection.close();
     }
 }
 
