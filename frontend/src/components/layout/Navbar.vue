81298146da1abde8a12153fc9b27877300a04cb4
RanderGabriel <randergabriel98@gmail.com>
2020-08-13 20:18:17 -0300

    Adicionando links na nav

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -1,13 +1,21 @@
 <template>
     <nav class="navbar">
+        <router-link to="/login">Login</router-link>
+        <router-link to="/signup">Cadastro</router-link>
+        <router-link to="/profile">Perfil</router-link>
     </nav>
 </template>
 
 <style lang="scss" scoped>
 
     .navbar {
+        display: flex;
         background-color: black;
         height: 60px;
+    }
+
+    a {
+        margin: 16px;
     }
 
     @include when($desktop) {
