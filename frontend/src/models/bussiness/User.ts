f7f8c94b21f74e312fa3833302881b9cff93e3ed
Eduardo Oliveira <eduveraneio@gmail.com>
2020-09-07 21:35:27 -0300

    Tratamento de URL para Resetar Senha

--- frontend/src/models/bussiness/User.ts
+++ frontend/src/models/bussiness/User.ts
@@ -3,6 +3,7 @@ export default class User {
     public email: string;
     public password: string;
     public passwordConfirmation?: string;
+    public passwordResetToken?: string;
     public phoneNumber?: number;
     public name?: string;
     public id?: string;
@@ -13,6 +14,7 @@ export default class User {
         this.email = entity.email;
         this.password = entity.password;
         this.passwordConfirmation = entity.passwordConfirmation;
+        this.passwordResetToken = entity.passwordResetToken;
         this.profile = entity.profile;
         this.id = entity.id;
         this.token = entity.token;
