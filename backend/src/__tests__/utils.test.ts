5f2299b38c30aa2a785ca8e0f564e0a68431c79c
RanderGabriel <randergabriel98@gmail.com>
2020-09-07 21:59:05 -0300

    Adicionando testes para função de hash

---
+++ backend/src/__tests__/utils.test.ts
@@ -0,0 +1,31 @@
+import * as bcrypt from "bcrypt";
+import { generateSaltedPassword } from "../utils";
+
+jest.mock("bcrypt", () => ({
+    genSalt: jest.fn(),
+    hash: jest.fn()
+}));
+
+test("it should create salted hash", async () => {
+    // @ts-ignore
+    bcrypt.genSalt.mockImplementation((_, cb) => cb(null, "salt"));
+    // @ts-ignore
+    bcrypt.hash.mockImplementation((pass, salt, cb) => cb(null, "hash"));
+
+    const saltedHash = await generateSaltedPassword("password");
+
+    expect(saltedHash).toEqual("hash");
+});
+
+test("it should throw error", async () => {
+    // @ts-ignore
+    bcrypt.genSalt.mockImplementation((_, cb) => cb("error"));
+    // @ts-ignore
+    bcrypt.hash.mockImplementation((pass, salt, cb) => cb("error"));
+
+    const fn = jest.fn();
+
+    const saltedHash = await generateSaltedPassword("password").catch(fn);
+
+    expect(fn).toBeCalledTimes(2);
+});
