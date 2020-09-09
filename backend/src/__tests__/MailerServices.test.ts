59dac33cebf99b2a5cc0a1b1c5931d213ebc9988
Eduardo Oliveira <eduveraneio@gmail.com>
2020-09-09 09:48:15 -0300

    Implementação de testes para a classe MailerService

---
+++ backend/src/__tests__/MailerServices.test.ts
@@ -0,0 +1,26 @@
+import { mailerService } from "../services/MailerService";
+import { User }from "../entity";
+import { getConnection } from "../../__mocks__/typeorm";
+
+const testUser: User = {
+    email: "teste@123.com",
+    id: 1,
+    name: "teste",
+    passwordHash: "123123",
+    passwordResetExpires: new Date(0),
+    passwordResetToken: "123214234",
+    phoneNumber: "123123123",
+    profile: {
+        name: "MECHANIC",
+        id: 1
+    },
+};
+
+describe("it should send mailer", () => {
+    test("it should send", async () => {
+        const mailer = await mailerService.send(testUser);
+        console.log(mailer);
+        expect(mailer).toEqual(true);
+
+    });
+});
