1f12d27c22486ac849945aaac7bd41ed074d7104
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-09-09 04:17:30 -0300

    Implementação dos testes do service do veiculo no front-end

--- frontend/src/services/__mocks__/HttpService.ts
+++ frontend/src/services/__mocks__/HttpService.ts
@@ -1,31 +1,33 @@
-import { httpService } from "../HttpService"
 
-const HttpService: any = jest.createMockFromModule('../HttpService')
+export default class HttpService {
+    private getResponse: any;
+    private postResponse: any;
 
-let getResponse: any = null
-function __setGetResponse(response: any){
-    getResponse = response 
+    constructor() {
+        this.getResponse = null
+        this.postResponse = null 
+    }
+    
+    __setGetResponse(response: any){
+        this.getResponse = response
+    }
+
+    __setPostResponse(response: any){
+        this.postResponse = response
+    }
+
+    async get(url: string, params: any = null) {
+        return new Promise((resolve, reject) => {
+            resolve(this.getResponse)
+        })
+    }
+    
+    async post<T>(url: string, data: T | null = null) {
+        return new Promise((resolve, reject) =>  {
+            resolve(this.postResponse)
+        })
+    }
+    
 }
 
-let postResponse: any = null
-function __setPostResponse(response: any) {
-    postResponse
-}
-
-async function get(url: string, params: any = null) {
-    return new Promise((resolve, reject) => {
-        resolve(getResponse.data)
-    })
-}
-
-async function post<T>(url: string, data: T | null = null) {
-    return new Promise((resolve, reject) =>  {
-        resolve(postResponse.data)
-    })
-}
-
-
-HttpService.__setGetResponse = __setGetResponse
-HttpService.__getPostResponse = __setPostResponse
-HttpService.post = post
-HttpService.get = get
+export const httpService = new HttpService()
--- frontend/tests/unit/services/VehicleService.spec.ts
+++ frontend/tests/unit/services/VehicleService.spec.ts
@@ -1,5 +1,61 @@
-describe("Teste de exemplo", () => {
-    test("Exemplo", () => {
-        expect(1 + 1).toBe(2)
+import VehicleService from '../../../src/services/VehicleService'
+import Vehicle from '../../../src/models/bussiness/Vehicle'
+
+jest.mock('../../../src/services/HttpService')
+
+const vehicleService = new VehicleService()
+const vehicle = {
+    id: 1, 
+    make: 'TestFactory',
+    model: 'Test',
+    year: '2030',
+    licensePlate: 'TES1010',
+    color: 'silver',
+    ownerId: 1
+}
+
+describe("VehicleService", () => {
+    
+    test("Get success", async () => {
+        require('../../../src/services/HttpService').httpService.__setGetResponse({
+            data: vehicle
+        })
+
+        const response : any = await vehicleService.get(1)
+        expect(response.id).toBe(1)
+        expect(response.make).toBe('TestFactory')
+        expect(response.year).toBe('2030')
+        expect(response.licensePlate).toBe('TES1010')
+        expect(response.color).toBe('silver')
+        expect(response.ownerId).toBe(1)
     })
+
+    test("Get error", async () => {
+        require('../../../src/services/HttpService').httpService.__setGetResponse({
+            data: null
+        })
+
+        const response : any = await vehicleService.get(1)
+        expect(response).toBe(null)
+    })
+
+    test("Create success", async () => {
+         require('../../../src/services/HttpService').httpService.__setPostResponse({
+            data: vehicle
+        })
+
+        const response : any = await vehicleService.create(vehicle as Vehicle)
+        expect(response.id).toBe(1)
+    }) 
+    
+    test("Create error", async () => {
+        require('../../../src/services/HttpService').httpService.__setPostResponse({
+           data: null
+       })
+
+       const response : any = await vehicleService.get(1)
+       expect(response).toBe(null)
+   })    
+
+   
 })
