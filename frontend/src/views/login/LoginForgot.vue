8dc2c02e8d4908454e5663ae8ac7d3ba05dfbb2d
Eduardo Oliveira <eduveraneio@gmail.com>
2020-10-13 16:36:11 -0300

    Adicionando template para recuperação de senha.

---
+++ frontend/src/views/login/LoginForgot.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="ml-2 mr-2 mx-2">
+        <form>
+            <div class="mb-5">
+                <div class="field">
+                    <label class="label">Informe seu e-mail para recuperação</label>
+                    <div class="control">
+                        <input class="input" type="text">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <input name="user" type="radio" value="DRIVER" > Sou motorista
+                        <input name="user" type="radio" value="MECHANIC" > Sou mecânico
+                    </div>
+                </div>
+            </div>
+            <div>
+                <button class="button is-primary has-text-weight-bold is-fullwidth" :class="{'is-loading': isSending}" type="submit" :disabled="isSending">
+                    RECUPERAR
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
