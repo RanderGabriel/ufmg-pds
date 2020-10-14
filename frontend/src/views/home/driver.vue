9a1323915158070802e14e289ade6d367c5edbb6
Brian Aikau <brianaikau@gmail.com>
2020-10-14 14:06:51 -0300

    Melhoria de layout na página inicial do motorista

--- frontend/src/views/home/driver.vue
+++ frontend/src/views/home/driver.vue
@@ -5,61 +5,63 @@
             :mapConfig="mapConfig"
             apiKey="AIzaSyAk6DVhHF0mAdjhSVX5ymZO2Kdj-iCE-q4"/>
         <div class="is-round control box p-8 m-4">
-            <p class="heading">Precisa de ajuda?</p>
-            <form class="has-background-white" v-if="!isWaiting && !mechanicFound?.id" @submit.prevent="onSubmit()">
-                <div class="field is-primary">
-                    <div class="control">
-                        <label class="label">Mensagem</label>
-                        <input class="input" type="text" v-model="formData.message">
+            <template v-if="!isWaiting && !mechanicFound?.id">
+                <p class="heading">Precisa de ajuda?</p>
+                <form class="has-background-white"  @submit.prevent="onSubmit()">
+                    <div class="field is-primary">
+                        <div class="control">
+                            <label class="label">Mensagem</label>
+                            <input class="input" type="text" v-model="formData.message">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button v-bind:class="{ 'is-loading': isLoading }" class="button is-black is-rounded is-fullwidth" type="submit">ENVIAR SOLICITAÇÃO</button>
+                        </div>
+                    </div>
+                </form>
+            </template>
+            <div v-else-if="!mechanicFound?.id" class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                    <p class="title">Aguardando um mecânico...</p>
                     </div>
                 </div>
-                <div class="field">
-                    <div class="control">
-                        <button v-bind:class="{ 'is-loading': isLoading }" class="button is-black is-rounded is-fullwidth" type="submit">ENVIAR SOLICITAÇÃO</button>
+            </div>
+            <div v-else-if="started" class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                    <p class="title">Atendimento iniciado</p>
                     </div>
                 </div>
-        </form>
-        <div v-else-if="!mechanicFound?.id" class="level">
-            <div class="level-item has-text-centered">
-                <div>
-                <p class="title">Aguardando um mecânico...</p>
+                <div class="level-item has-text-centered">
+                    <div>
+                    <p class="subtitle">O mecânico está a caminho</p>
+                    </div>
+                </div>
+            </div>
+            <div v-else class="columns">
+                <div class="column">
+                    <p class="is-size-3 has-text-weight-semibold has-text-centered">Mecânico encontrado!!</p>
+                </div>
+                <div class="column">
+                    <div class="columns">
+                        <div class="column">
+                            <p class="heading">Nome</p>
+                            <p class="subtitle">{{ mechanicFound.name }}</p>
+                        </div>
+                        <div class="column">
+                            <p class="heading">Contato</p>
+                            <p class="subtitle"><a :href="`tel:${mechanicFound.phoneNumber}`">{{ mechanicFound.phoneNumber }}</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <button @click.prevent="onAccept()" class="m-2 button is-success is-rounded is-fullwidth">ACEITAR</button>
+                    <button  @click.prevent="onDeny()" class="m-2 button is-danger is-outlined is-rounded is-fullwidth">RECUSAR</button>
                 </div>
             </div>
         </div>
-        <div v-else-if="started" class="level">
-            <div class="level-item has-text-centered">
-                <div>
-                <p class="title">Atendimento iniciado</p>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                <p class="subtitle">O mecânico está a caminho</p>
-                </div>
-            </div>
-        </div>
-        <div v-else class="level m-4">
-            <div class="level-item has-text-centered">
-                <div>
-                <p class="subtitle">Mecânico encontrado!!</p>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                <p class="heading">Nome</p>
-                <p class="subtitle">{{ mechanicFound.name }}</p>
-                <p class="subtitle">Contato: <a :href="`tel:${mechanicFound.phoneNumber}`">{{ mechanicFound.phoneNumber }}</a></p>
-                <div class="field">
-                <div class="control">
-                    <button @click.prevent="onAccept()" class="m-2 button is-black is-rounded is-fullwidth">ACEITAR</button>
-                    <button  @click.prevent="onDeny()" class="m-2 button is-outline is-rounded is-fullwidth">RECUSAR</button>
-                </div>
-            </div>
-                </div>
-            </div>
-        </div>
-        </div>
-        
     </div>
 </template>
 
