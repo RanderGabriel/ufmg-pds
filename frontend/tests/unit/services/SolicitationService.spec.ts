9da87550191b2148fdbfe3eb14c7c68f39d49126
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-10-14 18:57:32 -0300

    teste solicitationService

---
+++ frontend/tests/unit/services/SolicitationService.spec.ts
@@ -0,0 +1,52 @@
+import SolicitationService, { solicitationService } from '../../../src/services/SolicitationService'
+import Solcitation from '../../../src/models/bussiness/Solicitation'
+
+jest.mock('../../../src/services/HttpService')
+
+
+const userService = new SolicitationService()
+
+describe("SolicitationService", () => {
+    
+    test("create", async () => {
+        require('../../../src/services/HttpService').httpService.__setPostResponse({
+            data: {
+                id: 1,
+                message: "HELP!",
+                driver: 1,
+                mechanic: 1,
+                createAt: Date(),
+                acceptedAt: Date(),
+                refusedAt: Date(),
+                finishedAt: Date(),
+                coordinates: "(0,0)"
+            }
+        })
+        const response = await solicitationService.create("HELP!");
+        
+        expect((response as Solcitation).message).toEqual("HELP!");
+        expect((response as Solcitation).id).toEqual(1);
+    })
+
+    test("accept", async () => {
+        require('../../../src/services/HttpService').httpService.__setPostResponse({})
+        const response = await solicitationService.accept(1);
+        
+        expect(response).toEqual(true)
+    })
+
+    test("cancel", async () => {
+        require('../../../src/services/HttpService').httpService.__setPostResponse({})
+        const response = await solicitationService.cancel(1);
+        
+        expect(response).toEqual(true)
+    })
+
+    test("finish", async () => {
+        require('../../../src/services/HttpService').httpService.__setPostResponse({})
+        const response = await solicitationService.finish(1);
+        
+        expect(response).toEqual(true)
+    })
+    
+})
