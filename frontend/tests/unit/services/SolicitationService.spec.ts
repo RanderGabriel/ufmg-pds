7599a949418cd5296b664c18e6e2f62050b57a08
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-10-14 19:08:06 -0300

    teste SolicitationService getAll e actives

--- frontend/tests/unit/services/SolicitationService.spec.ts
+++ frontend/tests/unit/services/SolicitationService.spec.ts
@@ -5,22 +5,35 @@ jest.mock('../../../src/services/HttpService')
 
 
 const userService = new SolicitationService()
+const solicitation_1 = {
+    id: 1,
+    message: "HELP!",
+    driver: 1,
+    mechanic: 1,
+    createAt: Date(),
+    acceptedAt: Date(),
+    refusedAt: Date(),
+    finishedAt: Date(),
+    coordinates: "(0,0)"
+}
+
+const solicitation_2 = {
+    id: 2,
+    message: "HELP ME!",
+    driver: 2,
+    mechanic: 2,
+    createAt: Date(),
+    acceptedAt: Date(),
+    refusedAt: Date(),
+    finishedAt: Date(),
+    coordinates: "(0,0)"
+}
 
 describe("SolicitationService", () => {
     
     test("create", async () => {
         require('../../../src/services/HttpService').httpService.__setPostResponse({
-            data: {
-                id: 1,
-                message: "HELP!",
-                driver: 1,
-                mechanic: 1,
-                createAt: Date(),
-                acceptedAt: Date(),
-                refusedAt: Date(),
-                finishedAt: Date(),
-                coordinates: "(0,0)"
-            }
+            data: {...solicitation_1}
         })
         const response = await solicitationService.create("HELP!");
         
@@ -33,6 +46,24 @@ describe("SolicitationService", () => {
         const response = await solicitationService.accept(1);
         
         expect(response).toEqual(true)
+    })
+
+    test("getAll", async () => {
+        require('../../../src/services/HttpService').httpService.__setGetResponse({
+            data: [solicitation_1, solicitation_2]
+        })
+        const response = await solicitationService.getAll();
+        console.log(response)
+        expect(response.data.length).toEqual(2)
+    })
+
+    test("actives", async () => {
+        require('../../../src/services/HttpService').httpService.__setGetResponse({
+            data: [solicitation_1, solicitation_2]
+        })
+        const response = await solicitationService.getAll();
+        console.log(response)
+        expect(response.data.length).toEqual(2)
     })
 
     test("cancel", async () => {
