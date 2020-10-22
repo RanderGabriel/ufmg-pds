0b71220ffc7475f71fba28317b314d5912ae4ae6
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-10-22 01:23:36 -0300

    add definição de tipo formData

--- frontend/src/views/home/driver.vue
+++ frontend/src/views/home/driver.vue
@@ -65,7 +65,7 @@ export default defineComponent({
     };
   },
   methods: {
-    async onSubmit(formData) {
+    async onSubmit(formData: {message: string}) {
       this.state = this.states[1]
       const message = formData.message;
       const created = await services.solicitationService.create(message);
