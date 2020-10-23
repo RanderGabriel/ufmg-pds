6127c0670286a8465597be0ba5d5ef719326d385
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-10-23 03:56:48 -0300

    removendo pacotes desnecessários

--- frontend/src/components/Rating.vue
+++ frontend/src/components/Rating.vue
@@ -1,49 +1,27 @@
 <template>
   <form>
-    <rating :items="items" legend="" :value="value" @change="update"></rating>
+    <p>Rating!</p>
   </form>
 </template>
  
 <script>
-import Rating from 'vue-bulma-rating'
+
  
 export default {
   components: {
-    Rating
+    
   },
  
   data () {
     return {
       value: 0,
       items: [
-        {
-          title: '5 Stars',
-          value: 5
-        },
-        {
-          title: '4 Stars',
-          value: 4
-        },
-        {
-          title: '3 Stars',
-          value: 3
-        },
-        {
-          title: '2 Stars',
-          value: 2
-        },
-        {
-          title: '1 Star',
-          value: 1
-        }
       ]
     }
   },
  
   methods: {
-    update (val) {
-      this.value = val
-    }
+   
   }
 }
 </script>
