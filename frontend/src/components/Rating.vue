dd1d89d05dd2289764d70d81b84f4a0f97a67c2a
AugustoPolizer <43486710+AugustoPolizer@users.noreply.github.com>
2020-10-24 14:38:46 -0300

    componente Rating finalizado

--- frontend/src/components/Rating.vue
+++ frontend/src/components/Rating.vue
@@ -2,53 +2,85 @@
   <div class="rating-container">
     <h2 class="title">Avaliação</h2>
     <div class="level is-mobile">
-       <div class="start-container" v-for="(starState, index) in startsState" :key="index">
-        <font-awesome-icon :class="{actived: starState}" icon="star" size="3x" @mouseenter="activeStar(index)" @mouseleave="deactivateStar" @click="rating(index)"/>
+      <div
+        class="start-container"
+        v-for="(starState, index) in startsState"
+        :key="index"
+      >
+        <font-awesome-icon
+          :class="{ actived: starState }"
+          icon="star"
+          size="3x"
+          @mouseenter="activeStar(index)"
+          @mouseleave="deactivateStar"
+          @click="rating(index)"
+        />
       </div>
     </div>
-   
-    <textarea class="textarea" placeholder="Opinião escrita (Opcional)"></textarea>
+
+    <textarea
+      class="textarea"
+      placeholder="Opinião escrita (Opcional)"
+    ></textarea>
     <div class="mt-4 mb-4 buttons">
-      <button class="button is-black is-rounded is-fullwidth" :disabled="rate === -1">Enviar</button>
-      <button class="button is-rounded is-fullwidth"> Pular </button>
+      <button
+        class="button is-black is-rounded is-fullwidth"
+        :disabled="grade === -1"
+        @click="subimitRating"
+      >
+        Enviar
+      </button>
+      <button class="button is-rounded is-fullwidth">Pular</button>
     </div>
   </div>
 </template>
  
 <script>
-
- 
 export default {
-  components: {
-    
-  },
- 
-  data () {
+  components: {},
+
+  data() {
     return {
-      rate: -1,
-      startsState: [false, false, false, false, false]
+      grade: -1,
+      startsState: [false, false, false, false, false],
+      evaluation: ""
+    };
+  },
+
+  methods: {
+    activeStar(index) {
+      if (this.grade === -1) {
+        this.startsState = [false, false, false, false, false].fill(
+          true,
+          0,
+          index + 1
+        );
+      }
+    },
+
+    deactivateStar() {
+      if (this.grade === -1) {
+        this.startsState = [false, false, false, false, false];
+      }
+    },
+
+    rating(index) {
+      this.grade = index + 1;
+      this.startsState = [false, false, false, false, false].fill(
+        true,
+        0,
+        index + 1
+      );
+    },
+
+    submitRating(){
+      this.$emit('rating', {
+        evaluation: this.evaluation,
+        grade: this.grade
+      })
     }
   },
- 
-  methods: {
-   activeStar(index){
-     if(this.rate === -1) {
-       this.startsState = [false, false, false, false, false].fill(true, 0, index + 1)
-     }
-   },
-
-   deactivateStar() {
-     if(this.rate === -1) {
-       this.startsState = [false, false, false, false, false];  
-     }
-   },
-
-   rating(index){
-      this.rate = index + 1;
-      this.startsState = [false, false, false, false, false].fill(true, 0, index + 1)
-   }
-  }
-}
+};
 </script>
 
 <style>
