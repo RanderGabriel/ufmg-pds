0d9833ecef94bfa8f1876ff3184a6938e7286aaf
Eduardo Oliveira <eduveraneio@gmail.com>
2020-11-03 11:32:52 -0300

    Adicionando componente MapLine.

---
+++ frontend/src/components/MapLine.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    // @ts-ignore
+    import { LINE_PATH_CONFIG } from '@/constants'
+
+    export default {
+        props: {
+            google: {
+                type: Object,
+                required: true
+            },
+            map: {
+                type: Object,
+                required: true
+            },
+            path: {
+                type: Array,
+                required: true
+            }
+        },
+
+        mounted() {
+            new this.google.maps.Polyline({
+                path: this.path,
+                map: this.map,
+                ...LINE_PATH_CONFIG
+            })
+        }
+    }
+
+</script>
