f3bf57abb2bc7f0b130d152b7cad4df60d90e876
Eduardo Oliveira <eduveraneio@gmail.com>
2020-11-03 11:09:44 -0300

    Adicionando componente MapMarker

---
+++ frontend/src/components/MapMarker.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    // @ts-ignore
+    import { POINT_MARKER_ICON_CONFIG } from '@/constants'
+
+    export default {
+    props: {
+        google: {
+        type: Object,
+        required: true
+        },
+        map: {
+        type: Object,
+        required: true
+        },
+        marker: {
+        type: Object,
+        required: true
+        }
+    },
+
+    mounted() {
+        new this.google.maps.Marker({
+        position: this.marker.position,
+        marker: this.marker,
+        map: this.map,
+        icon: POINT_MARKER_ICON_CONFIG
+        })
+    }
+    }
+</script>
